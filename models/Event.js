524b19e0bdb38678e4ccb641af5fd286eb4d4d3c
Mrenard72 <maxime.renard72@gmail.com>
2025-02-28 20:20:37 +0100

    testback

--- models/Event.js
+++ models/Event.js
@@ -1,10 +1,9 @@
+const mongoose = require('mongoose'); // ✅ Ajout de l'import Mongoose
+
 const eventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  location: {
-    latitude: { type: Number, required: true },
-    longitude: { type: Number, required: true },
-  },
+  location: { type: String, required: true },
   date: { type: Date, required: true }, 
   category: { type: String, enum: ['Sport', 'Culturel', 'Sorties', 'Culinaire'], required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
@@ -15,4 +14,3 @@ const eventSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 module.exports = mongoose.model('Event', eventSchema);
-
