0b999786a27f65bc6d7e20deefbee61a676ad332
Mrenard72 <maxime.renard72@gmail.com>
2025-03-04 17:03:15 +0100

    route put

--- routes/auth.js
+++ routes/auth.js
@@ -166,7 +166,7 @@ router.put('/change-password', authMiddleware, async (req, res) => {
 
     // Mise à jour du mot de passe dans la base de données
     user.password = hashedPassword;
-    await user.save();
+    await user.password.save();
 
     console.log("✅ Mot de passe mis à jour en base de données.");
 
