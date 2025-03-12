cc06d387e760479591cb1669a111bcc51dd11674
Mrenard72 <maxime.renard72@gmail.com>
2025-03-12 14:43:58 +0100

    push

--- routes/auth.js
+++ routes/auth.js
@@ -131,7 +131,8 @@ router.delete('/delete', authMiddleware, async (req, res) => {
       return res.status(404).json({ message: 'Utilisateur non trouvé' });
     }
 
-    await user.remove(); // Suppression de l'utilisateur
+    await User.findByIdAndDelete(userId); // Suppression propre avec Mongoose
+
     res.json({ message: 'Compte supprimé avec succès' });
 
   } catch (error) {
@@ -139,6 +140,7 @@ router.delete('/delete', authMiddleware, async (req, res) => {
     res.status(500).json({ message: 'Erreur serveur' });
   }
 });
+
 
 //===========================================================
 // Route pour modifier le nom d'utilisateur
